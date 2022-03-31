49de0d8247547c606477ad5ce6ca2c85953da48c
aadarshjha <jha.aadarsh@gmail.com>
2022-03-31 15:49:32 -0500

    switching to conda

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 # Centralizes experimentation
 # Aadarsh Jha
 
+import gym
+
+
 class ExecuteEnviornment:
     def __init__(self):
-        self.env = None
+        self.env = gym.make("CartPole-v0")
 
     def run(self):
-        pass
+        self.env.reset()
+        for _ in range(1000):
+            self.env.render()
+            # take a random action
+            self.env.step(self.env.action_space.sample())
+        self.env.close()
+
+
+if __name__ == "__main__":
+    env = ExecuteEnviornment()
+    env.run()
