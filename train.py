bfe884bf4308593f5d0a71dcf671ac842cfe7a98
aadarshjha <jha.aadarsh@gmail.com>
2022-03-31 16:56:58 -0500

    training works

--- train.py
+++ train.py
@@ -1,21 +1,21 @@
 import gym
 
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.deepq.policies import MlpPolicy
 from stable_baselines3 import DQN
 
-env = gym.make("CartPole-v1")
+env = gym.make("CartPole-v0")
 
-model = DQN(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("deepq_cartpole")
+model = DQN("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000, log_interval=4)
+model.save("dqn_cartpole")
 
-del model  # remove to demonstrate saving and loading
+del model # remove to demonstrate saving and loading
 
-model = DQN.load("deepq_cartpole")
+model = DQN.load("dqn_cartpole")
 
 obs = env.reset()
 while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
     env.render()
+    if done:
+      obs = env.reset()
